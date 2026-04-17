9b5710d91c50d60a3df5cfa841b1864a22febd7e
zakiya <zakiyaaziz31@gmail.com>
2026-04-17 21:22:41 +0530

    22

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
         duration: 1.2,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
         smoothWheel: true,
-        smoothTouch: false,
+        smoothTouch: true, /* Enable smooth touch for mobile */
+        touchMultiplier: 1.5
     });
 
     function raf(time) {
